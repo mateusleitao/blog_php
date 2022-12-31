d1c8cb3bc710f8ff6cb1d484e0ad5d5662efb5c5
mateusleitao <mateuselpidio0@gmail.com>
2022-12-31 00:07:06 -0300

    pagina principal mais ou menos configurada

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
-<?php
-include_once("templates/header.php");?>
-<h1>Olá mundo</h1>
-<?php
-include_once("templates/footer.php");?>
+<?php include_once("templates/header.php");?>
+<main>
+    <div id="title-container">
+        <h1>Blog Codar</h1>
+        <p>O seu blog de programaçao</p>
+    </div>
+    <div id="posts-container">
+        <?php foreach ($posts as $post ):?>
+            <div class="post-box">
+                <img src="<?= $BASE_URL ?>img/<?= $post['img'] ?>" alt="<?= $post['title']?>">
+                <h2 class="post-title">
+                    <a href="<?= $BASE_URL ?>post.php/id=<?= $post['id']?>"><?= $post['title']?></a>
+                </h2>
+                <p id="post-description"><?= $post['description']?></p>
+                <div id="tags-container">
+                    <?php foreach($post['tags'] as $tag): ?>
+                        <a href="#"><?= $tag ?></a>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        <?php endforeach;?>
+    </div>
+</main>
+<?php include_once("templates/footer.php");?>
 
